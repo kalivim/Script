701da987132722ae4b6e7611b51028072c1acb4a
kalivim <2296398909@qq.com>
2018-07-11 16:46:20 +0800

    Update manage.sh

--- Tomcat单机多实例管理/manage.sh
+++ Tomcat单机多实例管理/manage.sh
@@ -4,16 +4,18 @@
 # PATH=/opt/op/java/jdk1.8.0_172/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 # 应用名称
 app=
-
 # war包下载地址
 war_url=
-
 soft_dir="/opt/op"
 download_war_file="${soft_dir}/war_apps/${app}.war"
-
 export CATALINA_BASE="${soft_dir}/tomcat/$app"
 export CATALINA_HOME="${soft_dir}/tomcat"
-export JVM_OPTIONS="-Xms128m -Xmx512m -XX:PermSize=128m -XX:MaxPermSize=412m"
+export JVM_OPTIONS="-Xms528m -Xmx812m -Xmn328m"
+
+[ -d "${soft_dir}/war_apps" ] || mkdir ${soft_dir}/war_apps
+
+[ `which unzip` ] || yum -y -q install unzip >/dev/null 2>&1
+
 
 check(){
     PID=`ps aux|grep java|grep -w ${CATALINA_BASE}|awk '{print $2}'`
@@ -26,13 +28,11 @@ check(){
             echo -e "\033[93m $app is running but not provide services\033[0m"
         fi
         return 0
-
     else
         echo -e "\033[91m $app is dead\033[0m "
         return 1
     fi
 }
-
 start() {
     check
     if [ $? -eq 1 ];then
@@ -40,7 +40,6 @@ start() {
         $CATALINA_HOME/bin/startup.sh >/dev/null 2>&1
     fi
 }
-
 stop() {
     check
     if [ $? -eq 0 ];then
@@ -49,23 +48,16 @@ stop() {
         kill -9 $PID
     fi
 }
-
 update() {
     echo "下载文件"
     wget ${war_url} -O ${download_war_file} > /dev/null 2>&1
     if [ $? -eq 0 ];then
         cd ${CATALINA_BASE}/webapps/*/; unzip -q -o ${download_war_file} >/dev/null 2>&1
-    else
-        echo -e "\033[91m Download pkg error \033[0m"
-        exit 1
     fi
 }
-
 log() {
     tailf ${CATALINA_BASE}/logs/catalina.out
 }
-
-
 if [ $# != "0" ];then
     case "$1" in
         start)
